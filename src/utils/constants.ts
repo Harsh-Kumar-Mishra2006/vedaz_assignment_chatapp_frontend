ae819ab516c3681c9314cb6c0979cf1a379255d0
Harsh-Kumar-Mishra <mishrakumarharsh987@gmail.com>
2026-07-02 14:17:19 +0530

    fixed backend url configuration

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 // For development
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+export const API_URL = import.meta.env.VITE_API_URL;
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || API_URL;
 
 
